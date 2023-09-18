5079a9b4569142cff9cfa85dd0c8abd5bd057dc6
asmitul <asmitullah@gmail.com>
2023-09-18 12:15:14 +0800

    update

--- app/main.py
+++ app/main.py
@@ -20,8 +20,10 @@ if LOGS_LOCATE == "LOCAL":
 if LOGS_LOCATE == "REMOTE":
     from pymongo import MongoClient
     client = MongoClient(f'mongodb://{os.getenv("MONGODB_USER")}:{os.getenv("MONGODB_PASSWORD")}@{os.getenv("MONGODB_HOST")}:{os.getenv("MONGODB_PORT")}/?authMechanism=DEFAULT')
-    db = client[os.getenv("MONGODB_DATABASE_NAME")]
-    collection = db[os.getenv("MONGODB_LOGS_COLLECTION_NAME")]
+    db = client[os.getenv("APP_NAME")+os.getenv("MONGODB_LOGS_DATABASE_NAME")]
+    from datetime import datetime
+    current_date = datetime.now().strftime("%Y-%m-%d")
+    collection = db["log_"+current_date]
 
     logger = logging.getLogger(__name__)
     logger.setLevel(os.getenv("LOGGING_LEVEL"))
@@ -40,6 +42,20 @@ def main():
     logger.warning(" level warn : warning message but dont terminate issue!")
     logger.error(" level error : error message!")
     logger.critical("level critical : will be terminated!")
+
+
+    # main database
+    if LOGS_LOCATE == "REMOTE":
+        from pymongo import MongoClient
+        client = MongoClient(f'mongodb://{os.getenv("MONGODB_USER")}:{os.getenv("MONGODB_PASSWORD")}@{os.getenv("MONGODB_HOST")}:{os.getenv("MONGODB_PORT")}/?authMechanism=DEFAULT')
+        db = client[os.getenv("APP_NAME")+os.getenv("MONGODB_DATABASE_NAME")]
+        collection = db["collection"]
+
+        # insert data
+        collection.insert_one({
+            "id": 1,
+            "name": "test"
+        })
 
 if __name__ == "__main__":
     main()
